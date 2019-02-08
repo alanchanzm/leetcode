88eb6298600d001296a604f51fc67de672c0ce0c
Alan Chan <alanchanzm@outlook.com>
2019-02-09 00:56:38 +0800

    perf(034): searchRange

--- src/034.js
+++ src/034.js
@@ -4,22 +4,27 @@
  * @return {number[]}
  */
 function searchRange(nums, target) {
-  return [find(), find(false)];
+  const starting = find();
+  const ending = ~starting ? find(false) : starting; // 若 head === -1，就不需要再找右边界了
+  return [starting, ending];
 
-  function find(low = true, arr = nums) {
+  /**
+   * @param {boolean} findStarting 标记寻找左边界还是右边界
+   */
+  function find(findStarting = true) {
     let left = 0;
-    let right = arr.length - 1;
+    let right = nums.length - 1;
     let result = -1;
     while (left <= right) {
       const mid = (left + right) >> 1;
-      if (target === arr[mid]) {
+      if (target === nums[mid]) {
         result = mid;
-        if (low) right = mid - 1;
+        if (findStarting) right = mid - 1;
         else left = mid + 1;
         continue;
       }
 
-      if (target > arr[mid]) left = mid + 1;
+      if (target > nums[mid]) left = mid + 1;
       else right = mid - 1;
     }
 
